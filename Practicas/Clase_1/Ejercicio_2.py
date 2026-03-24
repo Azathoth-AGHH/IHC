f1e28e324a69d2816f852bf490fd5c182cfcbcb4
Alexis Gethsemani Hernández Hernández <alexisgethsemani@gmail.com>
2026-03-24 08:58:00 -0600

    Add files via upload

---
+++ Practicas/Clase_1/Ejercicio_2.py
@@ -0,0 +1,10 @@
+#Entrada de datos
+S = float(input("Ingrese el Angulo en grados sexagesimales: "))
+
+#Proceso de datos
+C=200*S/180
+R=3.1416*S/180
+
+#Salida de datos
+print("Angulo en grados centesimales: ",C)
+print("Angulo en radianes: ", R)
---
+++ Practicas/Clase_1/Ejercicio_3.py
@@ -0,0 +1,12 @@
+#Entrada de datos
+C = float(input("Ingrese la temperatura en grados Centigrados: "))
+
+#Procesamiento de datos
+K = 0 + 273.15
+R = (C * 1.8)+491.67
+F = (C *9/5)+32
+
+#Salida de datos
+print("Grados Kelvin: ",K)
+print("Grados Rankie",R)
+print("Grados Farentheint ",F)
